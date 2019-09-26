02e1ad91c11ea924dd3ce1f2735f6ea39ac5f9e8
Erik Moqvist <erik.moqvist@gmail.com>
2019-09-26 21:19:24 +0200

    File headers and remove dbg macro usage.

--- bitstream.c
+++ bitstream.c
@@ -1,5 +1,30 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Erik Moqvist
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "bitstream.h"
-#include <dbg.h>
 
 void bitstream_writer_init(struct bitstream_writer_t *self_p,
                            uint8_t *buf_p)
@@ -30,9 +55,7 @@ void bitstream_writer_append_bit(struct bitstream_writer_t *self_p,
 void bitstream_writer_append_u8(struct bitstream_writer_t *self_p,
                                 uint8_t value)
 {
-    dbg(value);
-    
-    if (dbg(self_p->bit_offset) == 0) {
+    if (self_p->bit_offset == 0) {
         self_p->buf_p[self_p->byte_offset] = value;
     } else {
         self_p->buf_p[self_p->byte_offset] |= (value >> self_p->bit_offset);
